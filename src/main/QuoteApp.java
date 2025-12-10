e874e1c86162598d36f9e7166a17948a37f01b10
Akhtar Ansari <80877589+Punkakz@users.noreply.github.com>
2025-12-10 11:53:26 +0530

    Create QuoteApp.java for Spring Boot application

---
+++ src/main/QuoteApp.java
@@ -0,0 +1,12 @@
+package com.akhtar.quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteApp.class, args);
+    }
+}
